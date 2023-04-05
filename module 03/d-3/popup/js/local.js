e356f650fbeb6515c5881e5c30c5d70a99b840a7
Ilona <mitsunaki@yandex.ru>
2023-04-05 17:32:26 +0300

    feat: add functionality, without refactoring

--- module 03/d-3/popup/js/local.js
+++ module 03/d-3/popup/js/local.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
   const playBtn = document.querySelector('.popup-local__play');
   const video = document.getElementById('local-video');
+  const plus10 = document.querySelector(".lc__plus10");
+  const plus20 = document.querySelector(".lc__plus20");
+  const minus10 = document.querySelector(".lc__minus10");
+  const minus20 = document.querySelector(".lc__minus20");
 
   playBtn?.addEventListener('click', () => {
     video?.play();
@@ -15,4 +19,28 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     playBtn.classList.toggle('hidden');
   })
+
+  plus10?.addEventListener('click', () => rewind('plus', 10));
+  plus20?.addEventListener('click', () => rewind('plus', 20));
+  minus10?.addEventListener('click', () => rewind('minus', 10));
+  minus20?.addEventListener('click', () => rewind('minus', 20));
+
+  function rewind (operator, value) {
+    const currentTime = video.currentTime;
+    const duration = video.duration;
+    const diff = duration - currentTime; 
+    if (operator === 'minus' && currentTime > 10 && value === 10) {
+      video.currentTime -= 10;
+    }
+    if (operator === 'minus' && currentTime > 20 && value === 20) {
+      video.currentTime -= 20;
+    }
+    if (operator === 'plus' && diff > 10 && value === 10) {
+      video.currentTime += 10;
+    }
+    if (operator === 'plus' && diff > 20 && value === 20) {
+      video.currentTime += 20;
+    }
+  }
 })
+
--- module 03/d-3/popup/js/youtube.js
+++ module 03/d-3/popup/js/youtube.js
@@ -1,6 +1,10 @@
 var tag = document.createElement("script");
 const youtubeButton = document.querySelector(".button__youtube");
 const popupClose = document.querySelector(".popup-youtube__close");
+const plus10 = document.querySelector(".yt__plus10");
+const plus20 = document.querySelector(".yt__plus20");
+const minus10 = document.querySelector(".yt__minus10");
+const minus20 = document.querySelector(".yt__minus20");
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
@@ -23,4 +27,32 @@ function onPlayerReady(event) {
   popupClose?.addEventListener("click", () => {
     event.target.stopVideo();
   });
+
+  plus10?.addEventListener('click', () => rewind('plus', 10));
+  plus20?.addEventListener('click', () => rewind('plus', 20));
+  minus10?.addEventListener('click', () => rewind('minus', 10));
+  minus20?.addEventListener('click', () => rewind('minus', 20));
+
+}
+
+function rewind (operator, value) {
+  const currentTime = player.getCurrentTime();
+  const duration = player.getDuration();
+  const diff = duration - currentTime; 
+  if (operator === 'minus' && currentTime > 10 && value === 10) {
+    player.seekTo(currentTime - 10, true);
+    player.playVideo();
+  }
+  if (operator === 'minus' && currentTime > 20 && value === 20) {
+    player.seekTo(currentTime - 20, true);
+    player.playVideo();
+  }
+  if (operator === 'plus' && diff > 10 && value === 10) {
+    player.seekTo(currentTime + 10, true);
+    player.playVideo();
+  }
+  if (operator === 'plus' && diff > 20 && value === 20) {
+    player.seekTo(currentTime + 20, true);
+    player.playVideo();
+  }
 }
