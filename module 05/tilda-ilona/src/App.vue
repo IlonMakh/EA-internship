11d4a51203a78009c10a016e6683c708a082a504
Ilona <mitsunaki@yandex.ru>
2023-04-17 20:07:07 +0300

    feat: add structure of vue app

---
+++ module 05/tilda-ilona/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header></Header>
+  <router-view />
+  <Footer></Footer>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="less">
+@import './less/main.less';
+</style>
---
+++ module 05/tilda-ilona/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <ul>
+      <li>
+        <a href="/">Home page</a>
+      </li>
+      <li>
+        <a href="/projects">Projects page</a>
+      </li>
+      <li>
+        <a href="/profile">Profile page</a>
+      </li>
+    </ul>
+  </header>
+</template>
